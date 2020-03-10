3be96539050526c8870bf0e9e002fbc78131929f
Jens Møller <jens@jensz12.com>
2020-03-10 14:29:20 +0100

    Skjul col når man søger

--- views/parkering.php
+++ views/parkering.php
@@ -5,8 +5,8 @@
 
 <div class="row">
 <?php foreach ($this->rests as $rest): ?>
-  <div class="col-sm-6">
-    <div class="card rest" data-search="<?php echo mb_strtolower($rest['navn']).' '.implode(' ', $rest['under_rests_lowercase']); ?>">
+  <div class="col-sm-6 rest" data-search="<?php echo mb_strtolower($rest['navn']).' '.implode(' ', $rest['under_rests_lowercase']); ?>">
+    <div class="card">
       <div class="card-body">
         <h2 class="card-title"><?php echo $rest['navn']; ?></h2>
         <h3><i class="fal fa-map-marker-check"></i> <a href="https://www.google.com/maps/dir/?api=1&origin=&destination=<?php echo urlencode($rest['adresse']); ?>"><?php echo $rest['adresse']; ?></a><?php if (!empty($rest['note'])) echo ' - '.$rest['note']; ?></h3>
