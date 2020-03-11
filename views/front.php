538cb37c58c0c37db6917404b30fc0dd3456583d
Jens <jens@jensz12.com>
2020-03-11 15:27:02 +0100

    By logoer

--- views/front.php
+++ views/front.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-sm-6">
     <div class="card">
-    <img src="/img/byer/aalborg.png" class="card-img-top" alt="Aalborg">
+    <img src="/img/byer/aalborg.png" class="card-img-top" alt="Aalborg" width="300px">
       <div class="card-body">
         <h5 class="card-title">Aalborg</h5>
         <p class="card-text">Dækker Aalborg & Nørresundby</p>
@@ -11,7 +11,7 @@
   </div>
   <div class="col-sm-6">
     <div class="card">
-    <img src="/img/byer/aarhus.png" class="card-img-top" alt="Aarhus">
+    <img src="/img/byer/aarhus.png" class="card-img-top" alt="Aarhus" width="300px">
       <div class="card-body">
         <h5 class="card-title">Aarhus</h5>
         <a href="/parkering/aarhus" class="btn btn-dark"><i class="fal fa-parking fa-fw"></i> Parkering</a>
@@ -20,7 +20,7 @@
   </div>
 <div class="col-sm-6">
     <div class="card">
-        <img src="/img/byer/hilleroed.png" class="card-img-top" alt="Hillerød">
+        <img src="/img/byer/hilleroed.png" class="card-img-top" alt="Hillerød" width="300px">
     <div class="card-body">
         <h5 class="card-title">Hillerød</h5>
         <p class="card-text">Dækker Hillerød, Allerød & Lillerød</p>
@@ -30,7 +30,7 @@
 </div>    
 <div class="col-sm-6">
     <div class="card">
-        <img src="/img/byer/cph.png" class="card-img-top" alt="København">
+        <img src="/img/byer/cph.png" class="card-img-top" alt="København" width="300px">
     <div class="card-body">
         <h5 class="card-title">København</h5>
             <p class="card-text">Dækker København, Frederiksberg, Tårnby & Kastrup</p>
@@ -40,7 +40,7 @@
 </div>
 <div class="col-sm-6">
     <div class="card">
-        <img src="/img/byer/cph.png" class="card-img-top" alt="København">
+        <img src="/img/byer/glostrup.svg" class="card-img-top" alt="Vestegnen" width="300px">
     <div class="card-body">
         <h5 class="card-title">Vestegnen</h5>
             <p class="card-text">Dækker Rødovre, Hvidovre, Glostrup, Brøndby, Høje-Taastrup, Greve & Ishøj</p>
@@ -50,7 +50,7 @@
 </div>
 <div class="col-sm-6">
     <div class="card">
-        <img src="/img/byer/cph.png" class="card-img-top" alt="København">
+        <img src="/img/byer/gentofte.svg" class="card-img-top" alt="Nord" width="300px">
     <div class="card-body">
         <h5 class="card-title">Nord for København</h5>
             <p class="card-text">Dækker Gentofte, Lyngby-Taarbæk, Gladsaxe, Herlev, Rudersdal & Hørsholm</p>
@@ -60,7 +60,7 @@
 </div>
 <div class="col-sm-6">
     <div class="card">
-        <img src="/img/byer/odense.png" class="card-img-top" alt="Odense">
+        <img src="/img/byer/odense.png" class="card-img-top" alt="Odense" width="300px">
     <div class="card-body">
         <h5 class="card-title">Odense</h5>
             <a href="/parkering/odense" class="btn btn-dark"><i class="fal fa-parking fa-fw"></i> Parkering</a>
@@ -69,7 +69,7 @@
 </div>
 <div class="col-sm-6">
     <div class="card">
-        <img src="/img/byer/roskilde.png" class="card-img-top" alt="Roskilde">
+        <img src="/img/byer/roskilde.png" class="card-img-top" alt="Roskilde" width="300px">
     <div class="card-body">
         <h5 class="card-title">Roskilde</h5>
             <a href="/parkering/roskilde" class="btn btn-dark"><i class="fal fa-parking fa-fw"></i> Parkering</a>
