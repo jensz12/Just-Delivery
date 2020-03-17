af18f2645852ca34a0c4dcb780b705b1598ea20f
Jens <jens@jensz12.com>
2020-03-17 13:11:48 +0100

    Partials

---
+++ partials/nav.php
@@ -0,0 +1,39 @@
+<nav class="navbar fixed-top navbar-dark navbar-expand-lg">
+<div class="container">
+<a class="navbar-brand" href="/">
+    <img src="/img/logo/je.png" width="30" height="30" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/aalborg"><i class="fal fa-parking fa-fw"></i> Aalborg</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/aarhus"><i class="fal fa-parking fa-fw"></i> Aarhus</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/hilleroed"><i class="fal fa-parking fa-fw"></i> Hillerød</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/cph"><i class="fal fa-parking fa-fw"></i> København</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/nord"><i class="fal fa-parking fa-fw"></i> Nord for København</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/vestegnen"><i class="fal fa-parking fa-fw"></i> Vestegnen</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/odense"><i class="fal fa-parking fa-fw"></i> Odense</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/parkering/roskilde"><i class="fal fa-parking fa-fw"></i> Roskilde</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
--- views/main.php
+++ views/main.php
@@ -40,45 +40,7 @@
 </head>
 <body>
 <header>
-<nav class="navbar fixed-top navbar-dark navbar-expand-lg">
-<div class="container">
-<a class="navbar-brand" href="/">
-    <img src="/img/logo/je.png" width="30" height="30" alt="">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/aalborg"><i class="fal fa-parking fa-fw"></i> Aalborg</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/aarhus"><i class="fal fa-parking fa-fw"></i> Aarhus</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/hilleroed"><i class="fal fa-parking fa-fw"></i> Hillerød</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/cph"><i class="fal fa-parking fa-fw"></i> København</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/nord"><i class="fal fa-parking fa-fw"></i> Nord for København</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/vestegnen"><i class="fal fa-parking fa-fw"></i> Vestegnen</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/odense"><i class="fal fa-parking fa-fw"></i> Odense</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/roskilde"><i class="fal fa-parking fa-fw"></i> Roskilde</a>
-      </li>
-    </ul>
-  </div>
-</div>
-</nav>
+<?php $this->partial('partials/nav.php'); ?>
 </header>
 <div class="container">
   <div class="row">
@@ -86,7 +48,7 @@
       <?php $this->yieldView(); ?>
       <div class="card">
         <div class="card-body">
-		    <p>Just Eat er <b>ikke</b> ansvarlige for indholdet på denne side. Fejl og forbedringer kan sendes til <a href="mailto:jens@jensz12.com">Jens</a>. Websiden er lavet med hjælp fra <a href="https://spirit55555.dk">Anders</a>. Restaurant listen er sidst opdateret i marts 2020.</p>
+		    <p>Just Eat er <b>ikke</b> ansvarlige for indholdet på denne side. Fejl og forbedringer kan sendes til <a href="mailto:jens@jensz12.com">Jens</a>. Websiden er lavet med hjælp fra <a href="https://spirit55555.dk">Anders</a>. Restaurant listen er sidst opdateret i marts <?php echo date("Y")?>.</p>
         </div>
       </div>
     </div>
