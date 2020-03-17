113851bca6cf0e85eb3d4315be97aa19a0d54ec2
Jens <jens@jensz12.com>
2020-03-17 14:59:08 +0100

    Nav & CSS updates

--- partials/nav.php
+++ partials/nav.php
@@ -9,30 +9,23 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/aalborg"><i class="fal fa-parking fa-fw"></i> Aalborg</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/aarhus"><i class="fal fa-parking fa-fw"></i> Aarhus</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/hilleroed"><i class="fal fa-parking fa-fw"></i> Hillerød</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/cph"><i class="fal fa-parking fa-fw"></i> København</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/nord"><i class="fal fa-parking fa-fw"></i> Nord for København</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/vestegnen"><i class="fal fa-parking fa-fw"></i> Vestegnen</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/odense"><i class="fal fa-parking fa-fw"></i> Odense</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/parkering/roskilde"><i class="fal fa-parking fa-fw"></i> Roskilde</a>
-      </li>
+
+      <li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fal fa-parking fa-fw"></i> Parkering</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="/parkering/aalborg"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Aalborg</a>
+							<a class="dropdown-item" href="/parkering/aarhus"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Aarhus</a>
+              <a class="dropdown-item" href="/parkering/hilleroed"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Hillerød</a>
+              <a class="dropdown-item" href="/parkering/cph"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> København</a>
+              <a class="dropdown-item" href="/parkering/nord"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Nord for København</a>
+              <a class="dropdown-item" href="/parkering/vestegnen"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Vestegnen</a>
+              <a class="dropdown-item" href="/parkering/odense"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Odense</a>
+              <a class="dropdown-item" href="/parkering/roskilde"><i class="fal fa-parking fa-fw" aria-hidden="true"></i> Roskilde</a>
+						</div>
+          </li>
+					<li class="nav-item">
+						<a class="nav-link" href="/bg"><i class="fal fa-mobile-android fa-fw"></i> Baggrunde</a>
+					</li>
     </ul>
   </div>
 </div>
