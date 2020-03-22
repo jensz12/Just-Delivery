76f5f534ac644d363e24f09cf6477688eaabebe4
Anders G. Jørgensen <anders@spirit55555.dk>
2020-03-22 17:43:41 +0100

    Remove info button

--- views/front.php
+++ views/front.php
@@ -6,7 +6,7 @@
 				<h1 class="card-title"><?php echo $city['name']; ?></h1>
 				<p class="card-text"><?php echo $city['desc']; ?></p>
 				<p class="card-text">Antal restauranter: <?php echo get_rests_count($city['id']); ?></p>
-				<a href="/parkering/<?php echo $city['url']; ?>" class="btn btn-outline-dark"><i class="fal fa-parking fa-fw"></i> Parkering</a><a href="/info/<?php echo $url; ?>" class="btn btn-dark disabled" role="button" aria-disabled="true"><i class="fal fa-info"></i> God Information</a>
+				<a href="/parkering/<?php echo $city['url']; ?>" class="btn btn-block btn-outline-dark"><i class="fal fa-parking fa-fw"></i> Parkering</a>
 			</div>
 		</div>
 	</div>
