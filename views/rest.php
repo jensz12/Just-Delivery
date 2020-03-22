5372827303906f2f71f4ad81a71d469bedebbecc
Jens <jens@jensz12.com>
2020-03-22 14:13:06 +0100

    Fixed a link

--- views/rest.php
+++ views/rest.php
@@ -3,6 +3,6 @@
 	<h2><i class="fal fa-map-marker-check"></i> <?php echo format_rest_address($this->rest) ?></h2>
 	<h3><i class="fal fa-phone"></i> <?php echo $this->rest['tel']; ?></h3>
 	<h4><i class="fal fa-parking"></i> <?php echo $this->rest['parking']; ?></h4>
-	<a href="#" class="btn btn-outline-dark"><i class="fal fa-map-marker-check"></i> Naviger til resaturanten</a>
+	<a href="<?php echo format_rest_nav_link($this->rest); ?>" class="btn btn-outline-dark"><i class="fal fa-map-marker-check"></i> Naviger til resaturanten</a>
 	<a href="tel:<?php echo $this->rest['tel']; ?>" class="btn btn-outline-dark"><i class="fal fa-phone"></i> Ring til resaturanten</a>
 </div>
