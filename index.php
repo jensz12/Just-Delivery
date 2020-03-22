a7bba2a91a0c1f95a12c0a9adf30c9bb0187834f
Jens <jens@jensz12.com>
2020-03-22 20:36:54 +0100

    Login & Admin routes
    Admin interface :eyes:

--- index.php
+++ index.php
@@ -18,6 +18,16 @@ $klein->respond('GET', '/', function($request, $response, $service) {
 	$service->render('views/front.php');
 });
 
+$klein->respond('GET', '/login', function($request, $response, $service) {
+	$service->title = 'Login';
+	$service->render('views/login.php');
+});
+
+$klein->respond('GET', '/admin', function($request, $response, $service) {
+	$service->title = 'Admin';
+	$service->render('views/admin.php');
+});
+
 $klein->respond('GET', '/baggrunde', function($request, $response, $service) {
 	$service->title = 'Baggrunde';
 	$service->backgrounds = get_backgrounds();
