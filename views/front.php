ee7261c436731beac7281bdf55eb9ce96fb58cb8
Jens <jens@jensz12.com>
2020-03-22 15:59:23 +0100

    Added rest count on front page

--- views/front.php
+++ views/front.php
@@ -5,6 +5,7 @@
 			<div class="card-body">
 				<h1 class="card-title"><?php echo $city['name']; ?></h1>
 				<p class="card-text"><?php echo $city['desc']; ?></p>
+				<p class="card-text">Antal restauranter: <?php echo get_rests_count($city['id']); ?></p>
 				<a href="/parkering/<?php echo $city['url']; ?>" class="btn btn-outline-dark"><i class="fal fa-parking fa-fw"></i> Parkering</a><a href="/info/<?php echo $url; ?>" class="btn btn-dark disabled" role="button" aria-disabled="true"><i class="fal fa-info"></i> God Information</a>
 			</div>
 		</div>
