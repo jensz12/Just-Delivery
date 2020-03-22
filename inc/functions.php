f8d07c948874034c5b8424584ca6db8548826965
Anders G. Jørgensen <anders@spirit55555.dk>
2020-03-22 15:26:13 +0100

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,9 +1,9 @@
 <?php
 function remove_accents($text) {
 	$text = mb_strtolower($text);
-	$text = str_replace(array('ê', 'é'), 'e', $text);
+	$text = str_replace(['ê', 'é'], 'e', $text);
 	$text = str_replace('ö', 'o', $text);
-	$text = str_replace(array('\'','’'), '', $text);
+	$text = str_replace(['\'','’'], '', $text);
 
 	return $text;
 }
