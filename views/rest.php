e81ad6dc53e468b8cf1da599636f48b8ef722699
Anders G. Jørgensen <anders@spirit55555.dk>
2020-03-22 14:30:27 +0100

    Add message about no parking info

--- views/rest.php
+++ views/rest.php
@@ -2,7 +2,7 @@
 	<h1><?php echo $this->rest['name']; ?></h1>
 	<h2><i class="fal fa-map-marker-check"></i> <?php echo format_rest_address($this->rest) ?></h2>
 	<h3><i class="fal fa-phone"></i> <?php echo $this->rest['tel']; ?></h3>
-	<h4><i class="fal fa-parking"></i> <?php echo $this->rest['parking']; ?></h4>
+	<h4><i class="fal fa-parking"></i> <?php if (!empty($this->rest['parking'])) echo $this->rest['parking']; else echo 'Ingen parkeringsinfo endnu&hellip;'; ?></h4>
 	<a href="<?php echo format_rest_nav_link($this->rest); ?>" class="btn btn-outline-dark"><i class="fal fa-map-marker-check"></i> Naviger til resaturanten</a>
 	<a href="tel:<?php echo $this->rest['tel']; ?>" class="btn btn-outline-dark"><i class="fal fa-phone"></i> Ring til resaturanten</a>
 </div>
