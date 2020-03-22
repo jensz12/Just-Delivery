ba2bf15f8034d6032ef7d9441872ee2dbc5e96be
Jens <jens@jensz12.com>
2020-03-22 21:45:53 +0100

    Login page
    :eyes:

---
+++ views/admin/login.php
@@ -0,0 +1,16 @@
+<div class="card">
+	<div class="card-body">
+		<h1 class="card-title">Login</h1>
+		<form>
+		<div class="form-group">
+			<label for="username">Brugernavn</label>
+			<input type="email" class="form-control" id="username" aria-describedby="username" placeholder="Indtast brugernavn">
+		</div>
+		<div class="form-group">
+			<label for="password">Adgangskode</label>
+			<input type="password" class="form-control" id="password" placeholder="Adgangskode">
+		</div>
+		<button type="submit" class="btn btn-primary">Login</button>
+		</form>
+	</div>
+</div>
