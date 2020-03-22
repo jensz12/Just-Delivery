59448637a43ad254f45dc2edc1c4aa054228e225
Jens <jens@jensz12.com>
2020-03-22 18:09:59 +0100

    Fixed button spacing on phones

--- views/rest.php
+++ views/rest.php
@@ -5,8 +5,14 @@
 	<h3><i class="fal fa-phone"></i> <?php echo $this->rest['tel']; ?></h3>
 	<?php endif; ?>
 	<h4><i class="fal fa-parking"></i> <?php if (!empty($this->rest['parking'])) echo $this->rest['parking']; else echo 'Ingen parkeringsinfo endnu&hellip;'; ?></h4>
-	<a href="<?php echo format_rest_nav_link($this->rest); ?>" class="btn btn-outline-dark"><i class="fal fa-map-marker-check"></i> Naviger til resaturanten</a>
-	<?php if (!empty($this->rest['tel'])): ?>
-	<a href="tel:<?php echo $this->rest['tel']; ?>" class="btn btn-outline-dark"><i class="fal fa-phone"></i> Ring til resaturanten</a>
-	<?php endif; ?>
+	<div class="row">
+		<div class="col-md-6">
+			<a href="<?php echo format_rest_nav_link($this->rest); ?>" class="btn btn-block btn-outline-dark mb-2 mb-md-0"><i class="fal fa-map-marker-check"></i> Naviger til resaturanten</a>
+		</div>
+		<div class="col-md-6">
+			<?php if (!empty($this->rest['tel'])): ?>
+			<a href="tel:<?php echo $this->rest['tel']; ?>" class="btn btn-block btn-outline-dark"><i class="fal fa-phone"></i> Ring til resaturanten</a>
+			<?php endif; ?>
+		</div>
+	</div>
 </div>
