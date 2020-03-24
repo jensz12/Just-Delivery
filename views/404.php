34b649ab092df659afe72b6577ddd3ec0a42eda2
Jens <jens@jensz12.com>
2020-03-24 11:16:45 +0100

    Cahnged 404 page to cards

--- views/404.php
+++ views/404.php
@@ -1,4 +1,6 @@
-<div class="jumbotron">
-	<h1>404</h1>
-	<h2>Siden blev ikke fundet</h2>
+<div class="card">
+	<div class="card-body">
+		<h1>404</h1>
+		<h2>Siden blev ikke fundet</h2>
+	</div>
 </div>
