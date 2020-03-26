73c6625e060fbbf2fcec41b70953aa9ccd4b650e
Jens <jens@jensz12.com>
2020-03-26 22:05:41 +0100

    Commented out the admin

--- partials/nav.php
+++ partials/nav.php
@@ -20,10 +20,10 @@
 					<a class="nav-link" href="/baggrunde"><i class="fal fa-mobile-android fa-fw"></i> Baggrunde</a>
 				</li>
 			</ul>
-			<ul class="navbar-nav navbar-right">
+			<!--<ul class="navbar-nav navbar-right">
 				<li class="nav-item">
 					<a class="nav-link" href="/admin"><i class="fal fa-sign-in"></i> Admin</a>
-				</li>
+				</li>-->
 			</ul>
 		</div>
 	</div>
