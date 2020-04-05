5c7eb48f8b9968963292dc3781ab1ad53f8c05da
Jens <jens@jensz12.com>
2020-04-05 22:23:20 +0200

    Basic email stuff

---
+++ views/admin/email/create_user.php
@@ -0,0 +1,5 @@
+Hej.
+
+Færdiggør oprettelsen af din bruger ved at klikke på følgende link:
+
+Dit brugernavn er:
---
+++ views/admin/email/forgtot_password.php
@@ -0,0 +1,4 @@
+Hej.
+
+Du har bedt om at få nulstillet dit password.
+Det kan du gøre ved at klikke på dette link
