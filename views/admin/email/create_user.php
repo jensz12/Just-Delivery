3a4390c7fcf7270dffc6f1dd31079ccea6a81f2d
Jens <jens@jensz12.com>
2020-04-05 23:00:53 +0200

    rename email

--- views/admin/email/create_user.php
+++
@@ -1,5 +0,0 @@
-Hej.
-
-Færdiggør oprettelsen af din bruger ved at klikke på følgende link:
-
-Dit brugernavn er:
--- views/admin/email/forgot_password.php
+++
@@ -1,4 +0,0 @@
-Hej.
-
-Du har bedt om at få nulstillet dit password.
-Det kan du gøre ved at klikke på dette link
