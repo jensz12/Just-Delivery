d4def1c704a8462d3edc42fe64b56bce049b0b85
Jens <jens@jensz12.com>
2020-05-05 01:25:45 +0200

    Bottom updates

--- views/tak.php
+++ views/tak.php
@@ -1,7 +1,7 @@
 <div class="card">
 	<div class="card-body">
 		<h1>Tak til følgende personner for at hjælpe med siden</h1>
-		<h4>Anders - For at hjælpe med design, kode, udvikling osv.</h4>
+		<h4><a href="https://spirit55555.dk">Anders</a> - For at hjælpe med design, kode, udvikling osv.</h4>
 		<h4>Emilie & Shaimaa - For at hjælpe med lister over alle restauranter og for at give mig ideen til at lave den til hele Danmark</h4>
 		<h4>August - For at hjælpe med at teste det hele og komme med idéer</h4>
 		<h4>Det gamle Team Aalborg - For at teste den oprindelige udgave</h4>
