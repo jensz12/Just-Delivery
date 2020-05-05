5cfc088e91e401e76a411f60315c766dc19cc584
Jens <jens@jensz12.com>
2020-05-05 16:18:55 +0200

    Forms sucks

--- views/fejl.php
+++ views/fejl.php
@@ -1,5 +1,5 @@
 <div class="card">
 	<div class="card-body">
-	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdlRYRRnr0cCVwjjJrnc0viUabi1-5s3jmMGXvcE6W7t4waCA/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Indlæser…</iframe>
+	<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeWrAaJ1r9xXnMkMb6Z8h6tSx722OSXXdXNHtasXhjPaCZCTw/viewform?embedded=true" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">Indlæser…</iframe>
 	</div>
 </div>
