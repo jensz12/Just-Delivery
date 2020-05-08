f0b1fd0d07469f4442ced5aca4e093363aaafbb9
Jens <jens@jensz12.com>
2020-05-08 15:39:28 +0200

    Thanks

--- views/tak.php
+++ views/tak.php
@@ -1,9 +1,10 @@
 <div class="card">
 	<div class="card-body">
 		<h1>Tak til følgende personer for at hjælpe med siden</h1>
+		<hr />
 		<h4><a href="https://spirit55555.dk">Anders</a> - For at hjælpe med design, kode, udvikling osv.</h4>
 		<hr/>
-		<h4>Emilie & Shaimaa - For at hjælpe med lister over alle restauranter og for at give mig ideen til at lave den til hele Danmark</h4>
+		<h4>Emilie, Shaimaa, Sara & Morten - For at hjælpe med lister over alle restauranter</h4>
 		<hr/>
 		<h4>August - For at hjælpe med at teste det hele og komme med idéer</h4>
 		<hr/>
