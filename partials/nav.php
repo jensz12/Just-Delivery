16a8dee96c5dd2978dfb14d98f206b36a1c5e280
Jens <jens@jensz12.com>
2020-05-18 11:55:19 +0200

    Use PDF icon

--- partials/nav.php
+++ partials/nav.php
@@ -28,7 +28,7 @@
 			</ul>
 			<ul class="navbar-nav navbar-right">
 				<li class="nav-item">
-					<a class="nav-link" href="https://static.jensz12.com/yuso.pdf"><i class="fal fa-car"></i> Yuso Guide</a>
+					<a class="nav-link" href="https://static.jensz12.com/yuso.pdf"><i class="fal fa-file-pdf"></i> Yuso Guide</a>
 				</li>
 			</ul>
 			<ul class="navbar-nav navbar-right">
