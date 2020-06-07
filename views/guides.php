0623e9d99c8d8139900bb5384e668d1389b828da
Jens <jens@jensz12.com>
2020-06-07 16:34:05 +0200

    Update guides.php

--- views/guides.php
+++ views/guides.php
@@ -4,7 +4,7 @@
 			<div class="card-body">
 				<h1 class="card-title">Yuso</h1>
 				<p class="card-text">Sådan installere og bruger du Yuso</p>
-				<a href="https://findparkering.nu/guides/yuso.pdf" class="btn btn-block btn-outline-dark"><i class="fal fa-file-pdf fa-fw"></i> Læs guiden</a>
+				<a href="https://findparkering.nu/guide/yuso.pdf" class="btn btn-block btn-outline-dark"><i class="fal fa-file-pdf fa-fw"></i> Læs guiden</a>
 			</div>
 		</div>
 	</div>
@@ -13,7 +13,7 @@
 			<div class="card-body">
 				<h1 class="card-title">McDelivery Yuso</h1>
 				<p class="card-text">How to McDelivery, når du benytter Yuso</p>
-				<a href="https://findparkering.nu/guides/McDelivery%20Yuso.pdf" class="btn btn-block btn-outline-dark"><i class="fal fa-file-pdf fa-fw"></i> Læs guiden</a>
+				<a href="https://findparkering.nu/guide/McDelivery%20Yuso.pdf" class="btn btn-block btn-outline-dark"><i class="fal fa-file-pdf fa-fw"></i> Læs guiden</a>
 			</div>
 		</div>
 	</div>
@@ -22,7 +22,7 @@
 			<div class="card-body">
 				<h1 class="card-title">McDelivery TMS</h1>
 				<p class="card-text">How to McDelivery, når du benytter TMS</p>
-				<a href="https://findparkering.nu/guides/McDelivery%20TMS.pdf" class="btn btn-block btn-outline-dark"><i class="fal fa-file-pdf fa-fw"></i> Læs guiden</a>
+				<a href="https://findparkering.nu/guide/McDelivery%20TMS.pdf" class="btn btn-block btn-outline-dark"><i class="fal fa-file-pdf fa-fw"></i> Læs guiden</a>
 			</div>
 		</div>
 	</div>
